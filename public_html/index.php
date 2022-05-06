0b8e18233dda089cb97f35b10ab4be78e26b0d2e
Codeilay <savin-io@yandex.ru>
2022-05-06 17:01:40 +0300

    WIP

---
+++ public_html/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Электронные испарители PLONQ</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <div id="wrapper" class="wrapper">
+
+        <nav id="topNavbar">
+            <div class="section-content">
+                <ul class="menu">
+                    <li><a class="link" href="/">Продукты</a></li>
+                    <li><a class="link" href="/">Приложение</a></li>
+                    <li><a class="link" href="/">Дистрибуция</a></li>
+                    <li><a class="link" href="/">Где купить</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <section class="section-first fullheight responsiveDuration">
+            <div id="trigger"></div>
+            <div id="first-lockup" class="first-lockup section-content">
+                <div class="image-wrapper">
+                    <img id="animate" src="images/l.png" class="overview-first-img">
+                </div>
+                <h1 class="image-first-logo"><img id="animate2" src="images/Plonq.png" class="overview-first-img"></h1>
+            </div>
+            <div class="description-wrapper">
+                Электронная сигарета, которая умеет удивлять.
+            </div>
+        </section>
+        
+        <section class="section-second" id="animate3">
+            <div id="trigger3" class="spacer s0"></div>
+            <div class="intro section-content">
+                <h2 class="future">Будущее наступило</h2>
+                <h3 class="title"><span>Plonq</span><span>имитирует</span><span>процесс</span></h3>
+                <div class="intro-description">Алгоритм изучит вашу привычку к курению и поможет плавно снизить интенсивность, вплоть до полного отказа, если вы этого хотите.</div>
+            </div>
+        </section>
+
+        <section class="section-image">
+            <div id="trigger2"></div>
+
+            <div class="section-content captions">
+                <div id="trigger4"></div>
+                <div id="trigger5"></div>
+                <div id="trigger6"></div>
+                <div class="inner">
+                    <p id="animate4" class="el el-1"><strong>Plonq X</strong> имитирует привычный процесс курения, а интеллектуальная система анализирует каждую затяжку и помогает управлять привычкой.</p>
+                    <p id="animate5" class="el el-2"><strong>Алгоритм изучит</strong> вашу привычку к курению и поможет плавно снизить интенсивность, вплоть до полного отказа, если вы этого хотите.</p>
+                    <p id="animate6" class="el el-3"><strong>Мы верим</strong> , что курение, может быть выбором, таким же безобидным, как чашка любимого кофе.</p>
+                </div>
+			</div>
+
+            <div class="overflow-wrapper">
+                <div class="images">
+                    <img src="images/plonqImg/1.png" id="changeImg" />
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/gsap.min.js"></script>
+    <script type="text/javascript" src="js/ScrollMagic.js"></script>
+    <script type="text/javascript" src="js/plugins/animation.gsap.js"></script>
+    <script type="text/javascript" src="js/debug.addIndicators.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+</body>
+</html>
---
+++ public_html/js/main.js
@@ -0,0 +1,79 @@
+window.onload = function() {
+    var el = document.getElementById("first-lockup");
+    el.classList.add("show");
+ };
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 600})
+// animate
+.setTween("#animate", {scale: 1.1})
+// .addIndicators({name: "1 (duration: 600)"}) // add indicators (requires plugin)
+.addTo(controller);
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 600})
+// animate
+.setTween("#animate2", {scale: 0.9})
+// .addIndicators({name: "1 (duration: 600)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+
+var images = [];
+for (i = 1; i < 119; i++) {
+    images.push("images/plonqImg/" + i + ".png");
+}
+
+var obj = {curImg: 0};
+
+// create tween
+var tween = TweenMax.to(obj, 1,
+    {
+        curImg: images.length - 1,	// animate propery curImg to number of images
+        roundProps: "curImg",				// only integers so it can be used as an array index
+        immediateRender: true,			// load first image automatically
+        ease: Linear.easeIn,			// show every image the same ammount of time
+        onUpdate: function () {
+          $("#changeImg").attr("src", images[obj.curImg]); // set the image source
+        }
+    }
+);
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: 1500})
+.setTween(tween)
+// .addIndicators({name: "2 (duration: 1500)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+// build scene
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger3"
+})
+.setTween("#animate3", 0.5, {backgroundColor: "white"}) // trigger a TweenMax.to tween
+// .addIndicators({name: "3 (duration: 0)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+// build scene
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger4"
+})
+.setTween("#animate4", 0.5, {opacity: 1, top: '15%'}) // trigger a TweenMax.to tween
+// .addIndicators({name: "4 (duration: 0)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+// build scene
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger5"
+})
+.setTween("#animate5", 0.5, {opacity: 1, top: '55%'}) // trigger a TweenMax.to tween
+// .addIndicators({name: "5 (duration: 0)"}) // add indicators (requires plugin)
+.addTo(controller);
+
+// build scene
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#trigger6"
+})
+.setTween("#animate6", 0.5, {opacity: 1, bottom: '25%'}) // trigger a TweenMax.to tween
+// .addIndicators({name: "6 (duration: 0)"}) // add indicators (requires plugin)
+.addTo(controller);
